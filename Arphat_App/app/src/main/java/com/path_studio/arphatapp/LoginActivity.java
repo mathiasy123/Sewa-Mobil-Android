e67dd832a2cd94209fed997cc7902f0e1f594c3c
PatriciaFiona <patriciafiona3@gmail.com>
2019-08-18 23:38:07 +0700

    Autentikasi dengan akun Google
    Hal yang ditambahkan:
    -autentikasi google
    
    Hal yang kurang:
    -saat sign in dengan google belum ditambahkan data dirinya ke database di firebase

--- Arphat_App/app/src/main/java/com/path_studio/arphatapp/LoginActivity.java
+++ Arphat_App/app/src/main/java/com/path_studio/arphatapp/LoginActivity.java
@@ -3,27 +3,40 @@ package com.path_studio.arphatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
-
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
     private Button login;
+    private ImageView mTwitter, mGoogle, mFacebook;
     private TextView register;
     private EditText mEmail, mPassword;
 
@@ -32,10 +45,28 @@ public class LoginActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
+    private View mProgressView;
+
+    private static final String TAG = "GoogleSignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+    public GoogleApiClient mGoogleApiClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -58,35 +89,21 @@ public class LoginActivity extends AppCompatActivity {
         mPassword = (EditText) findViewById(R.id.password);
 
         register = (TextView) findViewById(R.id.register_btn);
+
+        mTwitter = (ImageView) findViewById(R.id.twitter_btn);
+        mGoogle = (ImageView) findViewById(R.id.google_btn);
+        mFacebook = (ImageView) findViewById(R.id.fb_btn);
+
         login = (Button) findViewById(R.id.signin_btn);
 
-        //Direct ke halaman register
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(i);
-                finish();
-            }
-        });
+        mProgressView = findViewById(R.id.login_progress);
 
-        //proses Login
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String email = mEmail.getText().toString();
-                final String password = mPassword.getText().toString();
 
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this, "Sign in Error", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        });
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+        mFacebook.setOnClickListener(this);
+        mTwitter.setOnClickListener(this);
+        mGoogle.setOnClickListener(this);
 
     }
 
@@ -100,6 +117,105 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_btn:
+                login();
+                break;
+            case R.id.register_btn:
+                register();
+                break;
+            case R.id.twitter_btn:
+                signIn_Twitter();
+                break;
+            case R.id.google_btn:
+                signIn_Google();
+                break;
+            case R.id.fb_btn:
+                signIn_Facebook();
+                break;
+        }
+    }
+
+    private void register(){
+        Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void login(){
+        final String email = mEmail.getText().toString();
+        final String password = mPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Sign in Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void signIn_Twitter(){
+
+    }
+
+    private void signIn_Google(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signIn_Facebook(){
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                updateUI(null);
+            }
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(this, MainPageActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        //showProgressDialog();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+                //hideProgressDialog();
+            }
+        });
     }
 
 }
--- Arphat_App/app/src/main/java/com/path_studio/arphatapp/MainPageActivity.java
+++ Arphat_App/app/src/main/java/com/path_studio/arphatapp/MainPageActivity.java
@@ -2,13 +2,37 @@ package com.path_studio.arphatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    private Button mLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        mLogout = (Button) findViewById(R.id.signout_btn);
+
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //sign out
+                FirebaseAuth.getInstance().signOut();
+
+                //ganti halaman
+                Intent i = new Intent(MainPageActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+                return;
+            }
+        });
+
     }
 }
