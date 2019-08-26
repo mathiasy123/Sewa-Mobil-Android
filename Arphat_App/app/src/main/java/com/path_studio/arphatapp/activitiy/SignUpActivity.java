7c834295b4e4061e88fa5a5f15e5c5505b9343c5
PatriciaFiona <patriciafiona3@gmail.com>
2019-08-26 15:35:59 +0700

    Penggunaan validasi recaptcha di proses Sign Up manual

--- Arphat_App/app/src/main/java/com/path_studio/arphatapp/activitiy/SignUpActivity.java
+++ Arphat_App/app/src/main/java/com/path_studio/arphatapp/activitiy/SignUpActivity.java
@@ -6,13 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +29,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.path_studio.arphatapp.R;
 import com.path_studio.arphatapp.check_internet_connection;
 
-public class SignUpActivity extends AppCompatActivity {
+import java.util.concurrent.Executor;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button register;
     private TextView login;
@@ -32,6 +41,7 @@ public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     private DatabaseReference mDatabase;
+    private static final String TAG = "SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,50 +83,8 @@ public class SignUpActivity extends AppCompatActivity {
         login = (TextView) findViewById(R.id.login_btn);
         register = (Button) findViewById(R.id.signup_btn);
 
-        //Direct ke halaman login
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(i);
-                finish();
-            }
-        });
-
-        //proses Sign Up
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String username = mUsername.getText().toString();
-                final String email = mEmail.getText().toString();
-                final String password = mPassword.getText().toString();
-
-                //check apakah semua sudah terisi
-                if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password) && TextUtils.isEmpty(username)){
-                    Toast.makeText(SignUpActivity.this, "Please Fill All Form before Submit", Toast.LENGTH_SHORT).show();
-                }else if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(username)){
-                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-                                Toast.makeText(SignUpActivity.this, "Sign up Error", Toast.LENGTH_SHORT).show();
-                            }else{
-                                String user_id = mAuth.getCurrentUser().getUid();
-
-                                DatabaseReference current_user_db = mDatabase.child("Users").child("Customers").child(user_id);
-                                current_user_db.child("Username").setValue(username);
-                                current_user_db.child("Email").setValue(email);
-                                current_user_db.child("Password").setValue(password);
-                                current_user_db.child("SignUp Method").setValue("Email&Password");
-                            }
-                        }
-                    });
-                }else if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(username)){
-                    Toast.makeText(SignUpActivity.this, "Username, Email, or Password is Still Empty", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
 
     }
 
@@ -132,4 +100,76 @@ public class SignUpActivity extends AppCompatActivity {
         mAuth.removeAuthStateListener(firebaseAuthListener);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_btn:
+                Intent i = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+                break;
+            case R.id.signup_btn:
+                SafetyNet.getClient(this).verifyWithRecaptcha("6LeW2LQUAAAAAFreXTKjdwXoM-CCvir_Vjk_qk6I")
+                        .addOnSuccessListener( this,
+                                new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
+                                    @Override
+                                    public void onSuccess(SafetyNetApi.RecaptchaTokenResponse response) {
+                                        // Indicates communication with reCAPTCHA service was
+                                        // successful.
+                                        String userResponseToken = response.getTokenResult();
+                                        if (!userResponseToken.isEmpty()) {
+                                            // Validate the user response token using the
+                                            // reCAPTCHA siteverify API.
+
+                                            final String username = mUsername.getText().toString();
+                                            final String email = mEmail.getText().toString();
+                                            final String password = mPassword.getText().toString();
+
+                                            //check apakah semua sudah terisi
+                                            if(TextUtils.isEmpty(email) && TextUtils.isEmpty(password) && TextUtils.isEmpty(username)){
+                                                Toast.makeText(SignUpActivity.this, "Please Fill All Form before Submit", Toast.LENGTH_SHORT).show();
+                                            }else if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(username)){
+                                                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                                        if(!task.isSuccessful()){
+                                                            Toast.makeText(SignUpActivity.this, "Sign up Error", Toast.LENGTH_SHORT).show();
+                                                        }else{
+                                                            String user_id = mAuth.getCurrentUser().getUid();
+
+                                                            DatabaseReference current_user_db = mDatabase.child("Users").child("Customers").child(user_id);
+                                                            current_user_db.child("Username").setValue(username);
+                                                            current_user_db.child("Email").setValue(email);
+                                                            current_user_db.child("Password").setValue(password);
+                                                            current_user_db.child("SignUp Method").setValue("Email&Password");
+                                                        }
+                                                    }
+                                                });
+                                            }else if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(username)){
+                                                Toast.makeText(SignUpActivity.this, "Username, Email, or Password is Still Empty", Toast.LENGTH_SHORT).show();
+                                            }
+
+                                        }
+                                    }
+                                })
+                        .addOnFailureListener( this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                if (e instanceof ApiException) {
+                                    // An error occurred when communicating with the
+                                    // reCAPTCHA service. Refer to the status code to
+                                    // handle the error appropriately.
+                                    ApiException apiException = (ApiException) e;
+                                    int statusCode = apiException.getStatusCode();
+                                    Log.d(TAG, "Error: " + CommonStatusCodes
+                                            .getStatusCodeString(statusCode));
+                                } else {
+                                    // A different, unknown type of error occurred.
+                                    Log.d(TAG, "Error: " + e.getMessage());
+                                }
+                            }
+                        });
+                break;
+        }
+    }
 }
